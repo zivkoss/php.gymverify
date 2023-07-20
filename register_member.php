e4c802ede0617f74a55201d9c9dcce13da73785e
zivkoss <zivcoss@yahoo.com>
2023-07-20 09:12:57 +0200

    (first_name, last_name, email, phone_number, photo_path, training_plan_id, trainer_id, access_card_pdf_path) added

--- register_member.php
+++ register_member.php
@@ -5,4 +5,6 @@ require_once 'config.php';
 $sql = "INSERT INTO members
         (first_name, last_name, email, phone_number, photo_path, training_plan_id, trainer_id, access_card_pdf_path)
         VALUES 
-  (?, ?, ?, ?, ?, ?, ?, ?)";
+  (?, ?, ?, ?, ?, ?, ?, ?)"; 
+
+  // 
