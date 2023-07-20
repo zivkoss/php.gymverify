e99c121ebc55bd054de688ffc1c84b34c79b1c64
zivkoss <zivcoss@yahoo.com>
2023-07-20 09:12:16 +0200

    INSERT INTO members (first_name, last_name, email, phone_number, photo_path, training_plan_id, trainer_id, access_card_pdf_path) VALUES ('John', 'Doe', 'john.doe@example.com', '123-456-7890', '/path/to/photos/john.jpg', 1, 101, '/path/to/cards/john_card.pdf'); added

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -23,17 +23,20 @@ if (!isset($_SESSION['admin_id'])) {
         <div class="row mb-5">
             <div class="col-md-6">
                 <h2>Register Member</h2>
-                <form action="register_member.php" method="post" enctype="multipart/form-data"></form>
+                <form action="register_member.php" method="post" enctype="multipart/form-data">
                 First Name: <input class="form-control" type="text" name="first_name"><br>
                 Last Name: <input class="form-control" type="text" name="last_name"><br>
                 Email: <input class="form-control" type="email" name="email"><br>
                 Phone Number: <input class="form-control" type="text" name="phone_number"><br>
                 Training Plan:
                 <select class="form-control" name="training_plan_id">
-                    
+                     <option value="" disabled selected>Training Plan</option>
+                     <option value="">12 sessions plan</option>
+                     <option value="">30 sessions plan</option>
                 </select><br>
-                <input type="hidden" name="photo_path" id="photoPathInput"> 
-                <div id="dropzone_upload" class="dropzone"></div>
+                <input type="hidden" name="photo_path" id="photoPathInput">
+                 
+                <div id="dropzone-upload" class="dropzone"></div>
 
                 <input class="btn btn-primary mt-3" type="submit" value="Register Member">
                 </form>
